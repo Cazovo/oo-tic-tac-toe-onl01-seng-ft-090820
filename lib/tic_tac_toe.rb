7ee431f90d4d39f60b6238411b710f7d6b2560e7
conner fortner <fortner.conner@yahoo.com>
2020-09-21 18:00:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,3 +38,51 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end  
+  
+  def turn_count
+    turn = 0 
+    @board.each do |index|
+      if index == "X"
+        turn += 1
+      end
+    end
+    return turn
+  end
+  
+  def
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
