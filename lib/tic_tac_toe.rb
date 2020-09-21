2f722f5f6ff7b47c78c852ee6e6b6dac3ea82bd0
conner fortner <fortner.conner@yahoo.com>
2020-09-21 17:56:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,3 +22,19 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
+  
+  def position_taken?(index)
+    !(@bard[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end  
